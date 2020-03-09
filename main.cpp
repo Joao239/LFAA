dc168221f8d402584da594cec597f586d7d380c6
Joao239 <61971961+Joao239@users.noreply.github.com>
2020-03-09 10:48:25 -0300

    Exercicio

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+# include <math.h>
+# include <locale.h>
+
+int main ()
+{
+
+	double v,t = 0.0;
+	while( t < 0.45){
+		printf("Insira o valor da moeda:\n");
+		scanf("%1f", &v);
+		if (v == 0.10 || v == 0.25){
+			t = t + v;
+		}
+		else {
+		printf("Valor da moeda não valido\n");
+		}
+		if (t >= 0.45){
+			printf("retire sua coca cola\n");
+		}
+	}
+}
+
+
